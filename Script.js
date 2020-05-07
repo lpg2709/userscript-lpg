ccc319ccb981d63bda9427327417a0491a8202cc
Leonardo Padilha Guarezi <38223894+lpg2709@users.noreply.github.com>
2020-05-07 10:22:34 -0300

    Add request_r, consts and comments code

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Request
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.5
 // @description  try to take over the world!
 // @author       You
 // @match      *://*/*
@@ -11,24 +11,60 @@
 
 var teste = 'aaa';
 
-(function () {
+(function() {
     'use strict';
-    window.request = function (method, url, data) {
+    /**********
+     * CONSTS *
+     **********/
+    window.request.GET = 'GET';
+    window.request.POST = 'POST';
+
+    /********************
+     * FUNCTIONS: PRINT *
+     ********************/
+    window.request = function (method, url, data){
         return new Promise(function (resolve, reject) {
             var req = new XMLHttpRequest();
-            req.responseType = 'json';
             req.open(method, url, true);
-            req.onreadystatechange = function () {
-                if (req.readyState == XMLHttpRequest.DONE) {
+            req.setRequestHeader("Authorization", "Bearer 123456");
+            req.responseType = 'json';
+            req.onreadystatechange = function(){
+                if(req.readyState == XMLHttpRequest.DONE){
                     console.log(JSON.stringify(req.response));
                 }
             }
-            if (method == 'POST') {
+            if(method == 'POST'){
                 req.send(data);
-            } else {
+            }else{
                 req.send(null);
             }
         });
     };
-    console.log(`request v1.0 working!\nHow use:\n%c request%c(%cmethod%c, %curl%c, %cdata%c);\nWhere: \n %cmethod%c: %cPOST %cor %cGET \n %curl%c: %chttp %cor %chttps\n %cdata%c: request body, only for %cPOST`, 'color: #66d9ef;', 'color: #000;', 'color: #fd971f;', 'color: #000;', 'color: #fd971f;', 'color: #000;', 'color: #fd971f;', 'color: #000;', 'color: #fd971f;', 'color: #000;', 'color: #a6e22e;', 'color: #000;', 'color: #a6e22e;', 'color: #fd971f;', 'color: #000;', 'color: #a6e22e;', 'color: #000;', 'color: #a6e22e;', 'color: #fd971f;', 'color: #000;', 'color: #a6e22e;');
+
+    /*********************
+     * FUNCTIONS: RETURN *
+     *********************/
+    window.request_r = function (method, url, data){
+        return new Promise(function (resolve, reject) {
+            var req = new XMLHttpRequest();
+            req.open(method, url, true);
+            req.setRequestHeader("Authorization", "Bearer 123456");
+            req.responseType = 'json';
+            req.onreadystatechange = function(){
+                if(req.readyState == XMLHttpRequest.DONE){
+                    resolve(JSON.stringify(req.response));
+                }
+            }
+            if(method == 'POST'){
+                req.send(data);
+            }else{
+                req.send(null);
+            }
+        });
+    };
+
+    /*****************
+     * DOCUMENTATION *
+     *****************/
+    console.log(`request v1.0 working!\nHow use:\n%c request%c(%cmethod%c, %curl%c, %cdata%c);\nWhere: \n %cmethod%c: %cPOST %cor %cGET \n %curl%c: %chttp %cor %chttps\n %cdata%c: request body, only for %cPOST`, 'color: #66d9ef;', 'color: #000;', 'color: #fd971f;','color: #000;', 'color: #fd971f;','color: #000;', 'color: #fd971f;','color: #000;', 'color: #fd971f;','color: #000;', 'color: #a6e22e;','color: #000;', 'color: #a6e22e;','color: #fd971f;','color: #000;', 'color: #a6e22e;','color: #000;', 'color: #a6e22e;', 'color: #fd971f;','color: #000;', 'color: #a6e22e;');
 })();
