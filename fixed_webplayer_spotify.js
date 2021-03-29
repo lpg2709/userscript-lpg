b6bf55e06ecaab699b43add6756c25c8b4070d76
Leonardo Padilha Guarezi <38223894+lpg2709@users.noreply.github.com>
2021-03-29 11:24:38 -0300

    Update fixed_webplayer_spotify.js, auto fix position

--- fixed_webplayer_spotify.js
+++ fixed_webplayer_spotify.js
@@ -1,19 +1,24 @@
 // ==UserScript==
 // @name         Fixed webplayer Spotify
-// @version      1.0
-// @description  Put the player of webplayer Spotify fixed in the bottom
+// @version      2.0
+// @description  Put the player of webplayer Spotify fixed in the bottom, when window height is less then 590px
 // @match        https://open.spotify.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    window.fixedPlayer = function() {
-        var a = document.getElementsByClassName('Root__now-playing-bar')[0];
-        a.style.position = 'fixed';
-        a.style.bottom = '0';
-    }
-
-    // Your code here...
+    window.addEventListener('load', function () {
+        var wH = window.innerHeight;
+        if(wH < 590){
+            var interval = setInterval(function(){
+                var a = document.getElementsByClassName('Root__now-playing-bar')[0];
+                if(typeof(a) !== "undefined"){
+                    a.style.position = 'fixed';
+                    a.style.bottom = '0';
+                    clearInterval(interval);
+                }
+            }, 1000);
+        }
+    })
 })();
