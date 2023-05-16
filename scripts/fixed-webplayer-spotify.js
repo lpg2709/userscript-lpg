d522be91a528e56ae0027cba421e3883b2413038
Leonardo Padilha Guarezi <38223894+lpg2709@users.noreply.github.com>
2023-05-16 13:10:42 -0300

    Update fixed-webplayer-spotify.js 3.3

--- scripts/fixed-webplayer-spotify.js
+++ scripts/fixed-webplayer-spotify.js
@@ -1,26 +1,42 @@
 // ==UserScript==
 // @name         Fixed webplayer Spotify
-// @version      2.0
+// @version      3.0
 // @description  Put the player of webplayer Spotify fixed in the bottom, when window height is less then 590px
 // @author       lpg2709
 // @match        https://open.spotify.com/*
-// @icon         https://open.scdn.co/cdn/images/favicon32.8e66b099.png
+// @icon         https://open.spotifycdn.com/cdn/images/favicon16.1c487bff.png
 // @grant        none
 // ==/UserScript==
+
+function fixedPlayer() {
+	var a = document.querySelector(".Root__now-playing-bar");
+	if(typeof(a) !== "undefined"){
+		a.style.position = 'fixed';
+		a.style.bottom = '0';
+		a.style.backgroundColor = "#000"
+		return true;
+	}
+
+	return false;
+}
 
 (function() {
 	'use strict';
 	window.addEventListener('load', function () {
-		var wH = window.innerHeight;
-		if(wH < 590){
+		if(window.innerHeight < 600){
 			var interval = setInterval(function(){
-				var a = document.getElementsByClassName('Root__now-playing-bar')[0];
-				if(typeof(a) !== "undefined"){
-					a.style.position = 'fixed';
-					a.style.bottom = '0';
+				if(fixedPlayer()) {
 					clearInterval(interval);
 				}
 			}, 1000);
 		}
-	})
+	});
+
+	window.addEventListener("resize", (e) => {
+		if(window.innerHeight < 600){
+			fixedPlayer();
+		}
+	});
+
 })();
+
